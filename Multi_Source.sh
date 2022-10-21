4586666ce3251172ba0722aecd077cdd88c894be
Gregory Tucker <gregory.tucker@ess.eu>
2022-10-21 11:41:31 +0200

    [Add] Real source to sample-position time-of-flight to 'source' neutrons

--- Multi_Source.sh
+++ Multi_Source.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# The extend block adds the time-of-flight from the real source to the sample position
+# plus some estimate for the long-pulse width transmitted by the choppers.
+# For BIFROST the source to sample distance is ~160 m.
+# The ESS source has a maximum usable pulse width of ~3 microseconds, and BIFROST can pass them all in its coarsest setting
 snippet="""
 COMPONENT cassette_source_INDEX = Source_simple(yheight = sampleSizeY, xwidth = sampleSizeX, E0 = middleEnergy, dE = deltaEnergy,
                                             dist = focusdistance, focus_xw = slitSizeX, focus_yh = 2*tan(2 * DEG2RAD) * focusdistance)
 WHEN INDEX==cassette AT (0, 0, 0) RELATIVE Origin ROTATED (0, params_channel[INDEX]*RAD2DEG + slitAngle, 0) RELATIVE Origin
+EXTEND
+%{
+  double v2 = vx*vx + vy*vy + vz*vz;
+  t += (160.0 / sqrt(v2)) + 1.5e-3 * randpm1(); 
+%}
 """
 
 outfile="Multi_Source.snippet"
