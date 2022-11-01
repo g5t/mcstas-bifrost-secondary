2fef3392a4f5a27d52a61a1c42145c49e897fe9a
Gregory Tucker <gregory.tucker@ess.eu>
2022-11-01 16:49:52 +0100

    updates

--- Multi_Cassette.sh
+++ Multi_Cassette.sh
@@ -8,6 +8,11 @@ COMPONENT CassettePSD@C = PSD_monitor(nx=140, ny=90, filename=\"CassettePSD@C.da
                                      xwidth=params_analyzer[@C][0][2])
 WHEN @C==cassette AT (0, 0, 0.95*params_distances[@C][0][0]) RELATIVE CassetteArm@C
 
+COMPONENT CassetteEmon@C = E_monitor(nE=100, filename=\"CassetteEmon@C.dat\", restore_neutron=1,
+                                     xwidth=params_analyzer[@C][0][2], yheight=params_analyzer[@C][0][2],
+                                     Emin=middleEnergy-deltaEnergy, Emax=middleEnergy+deltaEnergy)
+WHEN @C==cassette AT (0, 0, 0.96*params_distances[@C][0][0]) RELATIVE CassetteArm@C
+
 // CASSETTE @C
 """
 
@@ -20,19 +25,24 @@ COMPONENT Analyzer@C@A = Monochromator_Rowland(
   NH = params_analyzer[@C][@A][0],
   zwidth = params_analyzer[@C][@A][1],
   yheight = params_analyzer[@C][@A][2],
-  mosaic = Mos, DM = 3.335, NV = 1,
+  mosaic = Mos, DM = 3.335, NV = 1, gap=0.002,
   show_construction=construction, focush=focus_type, RH=focus_radius,
   angle_h=params_analyzer[@C][@A][5] * RAD2DEG,
   source=\"Origin\", sink=\"DetPoint@C@A\"
 )
-WHEN noBS && @C==cassette AT (0, 0, 0) RELATIVE PREVIOUS ROTATED (0, -TAS[@A], 0) RELATIVE PREVIOUS
+WHEN noBS && @C==cassette AT (0, 0, 0) RELATIVE PREVIOUS ROTATED (0, -0.5*params_two_theta[@C][@A]*RAD2DEG, 0) RELATIVE PREVIOUS
 EXTEND %{
 noBS = (SCATTERED) ? 0 : 1;
 analyzer = (SCATTERED) ? @A : -1;
 %}
 
+COMPONENT AnalyzerEmon@C@A = E_monitor(nE=100, filename=\"AnalyzerEmon@C@A.dat\", restore_neutron=1,
+                                     xwidth=params_analyzer[@C][@A][2], yheight=params_analyzer[@C][@A][2],
+                                     Emin=middleEnergy-deltaEnergy, Emax=middleEnergy+deltaEnergy)
+WHEN noBS && @C==cassette AT (0, 0, 1.1*params_distances[@C][@A][0]) RELATIVE CassetteArm@C
+
 // ============================ SCATTERING ANGLE REORIENTATION ======================  
-COMPONENT DetAngle@C@A = Arm() WHEN @C==cassette && @A==analyzer AT (0, 0, 0) RELATIVE AnaPoint@C@A ROTATED (0, -(2*TAS[@A]), 0) RELATIVE AnaArm@C@A
+COMPONENT DetAngle@C@A = Arm() WHEN @C==cassette && @A==analyzer AT (0, 0, 0) RELATIVE AnaPoint@C@A ROTATED (0, -params_two_theta[@C][@A]*RAD2DEG, 0) RELATIVE AnaArm@C@A
 // =================================== DETECTOR ==================================== 
 COMPONENT DetPoint@C@A = Arm() WHEN @C==cassette && @A==analyzer AT (0, 0, params_distances[@C][@A][1]) RELATIVE DetAngle@C@A
 COMPONENT PSD@C@A = PSD_monitor(nx=140, ny=90, filename=\"PSD@C@A.dat\", restore_neutron=1,
@@ -63,7 +73,7 @@ outfile="Multi_Cassette.snippet"
 
 echo "//Auto generated file, Edit Multi_Cassette.sh instead!" > $outfile
 #for c in {0..8}; do
-for c in 0 1 2; do
+for c in 1; do
   echo "${per_cassette//@C/"$c"}" >> $outfile
   this_cassette="${per_pair//@C/"$c"}"
 #  for a in {0..4}; do
--- Multi_Source.sh
+++ Multi_Source.sh
@@ -4,13 +4,22 @@
 # For BIFROST the source to sample distance is ~160 m.
 # The ESS source has a maximum usable pulse width of ~3 microseconds, and BIFROST can pass them all in its coarsest setting
 snippet="""
-COMPONENT cassette_source_INDEX = Source_simple(yheight = sampleSizeY, xwidth = sampleSizeX, E0 = middleEnergy, dE = deltaEnergy,
-                                            dist = focusdistance, focus_xw = slitSizeX, focus_yh = 2*tan(2 * DEG2RAD) * focusdistance)
+COMPONENT cassette_source_INDEX = Source_simple(
+yheight = sampleSizeY, 
+xwidth = sampleSizeX, 
+E0 = middleEnergy, 
+dE = deltaEnergy, 
+flux = 1e9, 
+dist = slitDistance, 
+focus_xw = 1.01*slitSizeX, 
+focus_yh = 2*tan(2 * DEG2RAD) * slitDistance
+)
 WHEN INDEX==cassette AT (0, 0, 0) RELATIVE Origin ROTATED (0, params_channel[INDEX]*RAD2DEG + slitAngle, 0) RELATIVE Origin
 EXTEND
 %{
   double v2 = vx*vx + vy*vy + vz*vz;
-  t += (160.0 / sqrt(v2)) + 1.5e-3 * randpm1(); 
+//  t += (160.0 / sqrt(v2)) + 1.5e-3 * randpm1(); 
+  t += (160.0 / sqrt(v2));
 %}
 """
 
